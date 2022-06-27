ccd525af45db9e02b79c779fa76e195770ef8d37
Karina Reed <zarringis@gmail.com>
2022-06-27 14:29:22 +0300

    Вывод значения элементов массива array в обратном порядке, если длина…. Обновленно

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0505/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0505/Solution.java
@@ -5,25 +5,25 @@ import java.util.Scanner;
 /* 
 Reverse
 */
-//TODO как вывести в обратном порядке
 public class Solution {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
-        int [] array;
+        int[] array;
         int N = console.nextInt();
+        if (N <= 0)
+            return; //выйти из текущего метода
         array = new int[N];
         for (int i = 0; i < N; i++) {
             array[i] = console.nextInt(); // заполняет массив элементами
         }
-        for (int i = 0; i < N; i++) {
-            if (N % 2 == 1)
-            System.out.println(array[i]); // выводит массив в порядке ввода
-            else
-                for (int l = 0; l < N; l++){
-                    array[l] = N - l;
-                    System.out.println(array[l]); //нужно вывести в обратном порядке
-                }
-        }
 
+        if (N % 2 == 0)
+            for (int l = array.length - 1; l >= 0; l--) {
+                System.out.println(array[l]); //выводит в обратном порядке
+            }
+        else
+            for (int i = 0; i < N; i++) {
+                System.out.println(array[i]); // выводит массив в порядке ввода
+            }
     }
 }
